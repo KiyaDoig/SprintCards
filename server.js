b825eec8b511286ac756e60bab19ec6126500b48
Kiya Doig <KDoig@starrez.com>
2020-06-10 16:11:58 +1000

    Revert port back to 80 and update docker scripts for Tron team

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const nextApp = next({ dev })
 const nextHandler = nextApp.getRequestHandler()
 
-let port = 3000
+let port = 80
 
 let answers = {};
 
